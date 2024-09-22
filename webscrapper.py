c56a5948f8f0bb785ed10da079f9d40afbbdf514
Nadotti <nadottipedro5@gmail.com>
2024-09-22 20:02:46 -0300

    Aplicacao completa!

--- webscrapper.py
+++ webscrapper.py
@@ -1,7 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = input("Cole a url de algum site: ")
+url = "https://www.bawclothing.com.br/baw/vestuario/partes-de-cima/camisetas?page=3"
 
 response = requests.get(url)
 
@@ -9,6 +9,18 @@ if response.status_code == 200:
     processed = response.content
 
     soup = BeautifulSoup(processed, 'html.parser')
-    print(soup.title.string)
+    title = soup.title.text
+    spans = soup.find_all('span')
+    precos = []
+    receita = 0
+    for span in spans:
+        if 'x' not in span.text:
+            if 'R$' in span.text:
+                precos.append(span.string.strip("R$\xa0"))
+    
+    for preco in precos:
+        receita += int(preco.strip(',00'))
+    print(f"A empresa {title} faria R$ {receita} se vendesse cada um dos produtos em um mes.")
+
 else:
     print("Houve um problema com o programa")
