ce30ed57561b1ae3afcbb3559eb4ebec3e670f95
Nadotti <nadottipedro5@gmail.com>
2024-09-22 18:36:40 -0300

    Construcao simples da aplicacao, onde consigo pegar o titulo de uma pagina da web

--- webscrapper.py
+++ webscrapper.py
@@ -1,4 +1,14 @@
 import requests
 from bs4 import BeautifulSoup
 
-print('Deu tudo certo')
+url = input("Cole a url de algum site: ")
+
+response = requests.get(url)
+
+if response.status_code == 200:
+    processed = response.content
+
+    soup = BeautifulSoup(processed, 'html.parser')
+    print(soup.title.string)
+else:
+    print("Houve um problema com o programa")
